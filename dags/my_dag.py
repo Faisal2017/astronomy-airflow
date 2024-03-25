aaee495e075d0c91f114643cd33873aea4a54bfb
fal <faisal_a@live.co.uk>
2024-03-25 22:48:01 +0000

    add args required for pytest

--- dags/my_dag.py
+++ dags/my_dag.py
@@ -4,10 +4,17 @@ from include.helpers import print_msg
 
 from datetime import datetime
 
-
-with DAG('my_dag', start_date=datetime(2022, 1, 1),
-         schedule_interval='@daily', catchup=False) as DAG:
-
+with DAG(
+        dag_id='my_dag',
+        start_date=datetime(2022, 1, 1),
+        schedule_interval='@daily',
+        catchup=False,
+        tags=['my_tag'],
+        default_args={
+            "owner": "community",  # Defines the value of the "owner" column in the DAG view of the Airflow UI
+            "retries": 2,  # If a task fails, it will retry 2 times.
+        }
+) as DAG:
     task = PythonOperator(
         task_id='task',
         python_callable=print_msg

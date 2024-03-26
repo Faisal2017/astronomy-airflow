d076e377773d79b5e575dfe75064ed75aacffb39
fal <faisal_a@live.co.uk>
2024-03-26 05:26:29 +0000

    add to Taskflow API DAG exmaple

--- dags/taskflowapi_example.py
+++ dags/taskflowapi_example.py
@@ -1,19 +1,39 @@
-from airflow.decorators import dag, task
+import json
 from datetime import datetime
 
+from airflow.decorators import dag, task
 
-@dag(start_date=datetime(2023, 1, 1),
-     description='A simple tutorial DAG',
-     tags=['data_science'],
-     schedule='@daily',
-     catchup=False,
-     )
+
+@dag(
+    schedule=None,
+    start_date=datetime(2024, 1, 1),
+    catchup=False,
+    tags=["example"],
+)
 def taskflow_api_dag():
-    @task
-    def print_a():
-        print('hi from task a')
+    @task()
+    def extract():
+        data_string = '{"1001": 301.27, "1002": 433.21, "1003": 502.22}'
 
-    print_a()
+        order_data_dict = json.loads(data_string)
+        return order_data_dict
+
+    @task(multiple_outputs=True)
+    def transform(order_data_dict: dict):
+        total_order_value = 0
+
+        for value in order_data_dict.values():
+            total_order_value += value
+
+        return {"total_order_value": total_order_value}
+
+    @task()
+    def load(total_order_value: float):
+        print(f"Total order value is: {total_order_value:.2f}")
+
+    order_data = extract()
+    order_summary = transform(order_data)
+    load(order_summary["total_order_value"])
 
 
 taskflow_api_dag()
